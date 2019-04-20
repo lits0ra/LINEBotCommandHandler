0898056425a98c11338dd3c22f01b737519cb2ad
litsora <s0ralit@protonmail.com>
2019-04-20 12:14:15 +0900

    Add new line

--- command_handler.rb
+++ command_handler.rb
@@ -3,6 +3,7 @@ class CommandHandler
   def initialize(client)
     @client = client
   end
+
   def regist_keyword_and_function(keyword, function)
     @keyword_and_function.store(keyword, function)
   end
