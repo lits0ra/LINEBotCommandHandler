b6afc6d0bb5eda2a562eb6f7a17fb83f3b5d6ac5
litsora <s0ralit@protonmail.com>
2019-04-20 12:18:37 +0900

    Delete argument

--- event_handler.rb
+++ event_handler.rb
@@ -2,7 +2,7 @@ require 'line/bot'
 require './command_handler'
 
 class EventHandler < CommandHandler
-  def execute(events, client)
+  def execute(events)
     events.each do |event|
       case event
       when Line::Bot::Event::Message
